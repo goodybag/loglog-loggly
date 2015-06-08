506fdee9d081303b5d890af88f6ea518916edbd3
John Fawcett <jrf0110@gmail.com>
2015-06-08 09:02:25 -0500

    comments and client.log

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var loggly = require('loggly');
 
 module.exports = function( options ){
   var defaults = {
-  , json: true
+    json: true
   };
 
   for ( var key in defaults ){
@@ -11,9 +11,11 @@ module.exports = function( options ){
     }
   }
 
+  // Create an initial client to verify options
   var client = loggly.createClient( options );
 
   return function( entry ){
-    client.log( entry );
+    loggly.createClient( options );
+    client.log( entry, entry.parents.concat( entry.copmonent || [] ) );
   };
 }
